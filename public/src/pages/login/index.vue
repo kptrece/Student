836e64d62bff463affc3897cb433074e357b139b
JLipreso <lipreso.developer@gmail.com>
2025-02-09 23:22:12 +0800

    OK

--- public/src/pages/login/index.vue
+++ public/src/pages/login/index.vue
@@ -2,7 +2,32 @@
   <div>
     <SectionHeader/>
     <main class="main">
-      <div class="p-5 m-5"></div>
+      <div class="w-100 h-100">
+        <div class="h-100 d-flex justify-content-center align-items-center">
+          <div class="row">
+            <div class="card">
+              <div class="card-body">
+                <div style="max-width: 375px;">
+                  <h1 class="text-cenetr">Algo Spear</h1>
+                  <div class="px-5 mt-4">
+                      <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input class="form-control form-control-lg" type="text" />
+                      </div>
+                      <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input class="form-control form-control-lg" type="password" />
+                      </div>
+                      <div class="d-flex justify-content-end">
+                        <button class="btn btn-primary btn-lg w-50">Sign In</button>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
     <SectionFooter/>
   </div>
